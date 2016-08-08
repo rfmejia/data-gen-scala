0a9ab25183fc5b55d3ad2a959619fb8957ec211b
Akie Mejia <rfmejia@protonmail.com>
2016-08-08 12:51:54 +0900

    Decomposed generator structures

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ A generic data generator with support for JSON data.
 #### Attribution
 
 US name data adapted from the [US Census Bureau](http://www.census.gov/topics/population/genealogy/data/1990_census/1990_census_namefiles.html).
+
+Company name data adapted from [Wordlab](http://www.wordlab.com/archives/company-names-list/).
+
+[Country data](https://github.com/datasets/country-codes) and []() were adapted from [Open Knowledge International](http://data.okfn.org/roadmap/core-datasets).
--- src/main/scala/net/ximity/events/datagen/Generators.scala
+++ src/main/scala/net/ximity/events/datagen/Generators.scala
@@ -4,78 +4,117 @@ import java.time.Instant
 import java.util.UUID
 import scala.util.Random
 
-trait RandomGenerator {
-  val seed: Option[Long]
-  val rand = seed.map(s => new Random(s)).getOrElse((new Random))
 
-  val lorem: Lorem
+trait ListGenerator extends PrimitiveGenerator {
+  def range(start: Int = 0, stop: Int, step: Int = 1): Stream[Int] = (start to stop by step).toStream
+
+  def random[T](stream: Stream[T])(implicit r: Random): T = stream.drop(r.nextInt(stream.size - 1)).head
+
+  def repeat[T](min: Int, max: Option[Int], f: Int => T)(implicit r: Random): Stream[T] = {
+    val count = max.map(mx => integer(min, mx)) getOrElse min
+    (0 to count - 1).toStream.map(f)
+  }
 }
 
-trait Generator extends RandomGenerator {
+trait PrimitiveGenerator {
+  def bool(implicit r: Random): Boolean = r.nextBoolean()
 
-  def bool: Boolean = rand.nextBoolean()
+  def integer(min: Int = 0, max: Int = Int.MaxValue)(implicit r: Random): Int =
+    (r.nextInt(max - min) + min)
 
-  def city: String = ???
+  def float(min: Double = Double.MinValue, max: Double = Double.MaxValue)(implicit r: Random): Double =
+    (r.nextDouble() * max) + min
 
-  def company: String = ???
+  def gauss(mu: Double = 0.0f, sigma: Double = 1.0f) = ???
+}
 
-  def country(abbrev: Boolean = false): String = ???
+trait Generator {
+  def function[T](f: Random => T)(implicit r: Random): T = f(r)
+}
 
-  def date(min: Instant = Instant.MIN, max: Instant = Instant.MAX): Instant = {
-    val ms = ((rand.nextDouble * max.getEpochSecond) + min.getEpochSecond).toLong
-    Instant.ofEpochMilli(ms)
-  }
-
-  def domainZone: String = ???
-
-  def firstName(gender: Gender): String = ???
-
-  def float(min: Double = Double.MinValue, max: Double = Double.MaxValue): Double = ???
-
-  def function[T](f: Random => T): T = f(rand)
-
-  def gauss(mu: Double = 0, sigma: Double) = ???
-
+trait IdentifierGenerator {
   def guid: UUID = UUID.randomUUID()
-
-  def index(): Int
-
-  def integer(min: Int = Int.MinValue, max: Int = Int.MaxValue): Int = rand.nextInt(max)
-
-  def lorem(num: Int, loremType: LoremType): String = lorem.lorem(num, loremType)
 
   def objectId: String = ???
 
+}
+
+trait ContactInfoGenerator {
+  def city: String = ???
   def phone(format: String): String = ???
-
-  def random[T](stream: Stream[T]): Option[T] = stream.drop(rand.nextInt(stream.size - 1)).headOption
-
-  def range(start: Int = 0, stop: Int, step: Int = 1): Stream[Int] = (start to stop by step).toStream
-
-  //def repeat[T](min: Int, max: Option[Int]): T
-
   def state(): String = ???
-
   def street(): String = ???
-
-  def surname(): String = ???
 }
 
-object JsonGenerator extends Generator {
-  val seed = None
-  val lorem = new Lorem(3, 6, rand)
+case class CountryData(name: String, iso3166_1: String)
 
-  def index(): Int = ???
+trait CountryGenerator extends ListGenerator {
+  val countryData: Stream[CountryData]
+
+  def country: CountryData = random(countryData)
+
+  def domainZone: String = ???
+}
+
+sealed trait Gender
+case object Male extends Gender
+case object Female extends Gender
+
+trait NameGenerator extends ListGenerator {
+  val surnames: Stream[String]
+  val femaleFirstNames: Stream[String]
+  val maleFirstNames: Stream[String]
+
+  def normalize(s: String): String = {
+    @scala.annotation.tailrec
+    def loop(in: List[String], acc: List[String]): List[String] = in match {
+      case Nil => acc
+      case head :: tail =>
+        val name = head.head.toUpper + head.tail.toLowerCase
+        loop(tail, acc :+ name)
+    }
+
+    loop(s.split(" ").toList, List.empty).mkString(" ")
+  }
+
+  def firstName(gender: Option[Gender])(implicit r: Random): String = gender match {
+    case Some(Female) => normalize(random(femaleFirstNames))
+    case Some(Male) => normalize(random(maleFirstNames))
+    case None => normalize(random(femaleFirstNames ++ maleFirstNames))
+  }
+
+  def surname(): String = normalize(random(surnames))
+}
+
+trait CompanyNameGenerator extends ListGenerator {
+  val companyNames: Stream[String]
+
+  def company: String = random(companyNames)
 
 }
 
-class Lorem(val minLength: Int, val maxLength: Int, val rand: Random) {
+trait DateGenerator {
+  def date(min: Instant = Instant.MIN, max: Instant = Instant.MAX)(implicit r: Random): Instant = {
+    val ms = ((r.nextDouble * max.getEpochSecond) + min.getEpochSecond).toLong
+    Instant.ofEpochMilli(ms)
+  }
+}
+
+sealed trait LoremType
+case object letters extends LoremType
+case object words extends LoremType
+case object sentences extends LoremType
+case object paragraphs extends LoremType
+
+trait LoremGenerator {
+  val minLength: Int
+  val maxLength: Int
   assert(maxLength > minLength, "Lorem generator length must be non-zero.")
 
-  private def randomLength: Int = rand.nextInt(maxLength - minLength) * minLength
+  private def randomLength(implicit r: Random): Int = r.nextInt(maxLength - minLength) * minLength
 
-  def lorem(num: Int, loremType: LoremType): String = loremType match {
-    case letters => rand.nextString(num)
+  def lorem(num: Int, loremType: LoremType)(implicit r: Random): String = loremType match {
+    case letters => r.nextString(num)
     case words => (for(n <- 1 to num) yield lorem(randomLength, letters)).mkString(" ")
     case sentences =>
       val ss = for(n <- 1 to num) yield {
@@ -87,12 +126,14 @@ class Lorem(val minLength: Int, val maxLength: Int, val rand: Random) {
   }
 }
 
-sealed trait LoremType
-case object letters extends LoremType
-case object words extends LoremType
-case object sentences extends LoremType
-case object paragraphs extends LoremType
+object JsonGenerator extends PrimitiveGenerator with LoremGenerator {
+  implicit val r = new Random
 
-sealed trait Gender
-case object Male extends Gender
-case object Female extends Gender
+  val minLength: Int = 3
+  val maxLength = 6
+
+  def index(): Int = ???
+
+
+}
+
